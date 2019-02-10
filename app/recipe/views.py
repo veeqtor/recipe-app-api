4de7f92c490b43f2ef19e8573b1a1641e4b67bc2
Veeqtor <nwokeochavictor@gmail.com>
2019-02-10 16:23:03 +0100

    ch(refactor): Refactor recipe viewset

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -5,44 +5,35 @@ from core.models import Tag, Ingredient
 from recipe import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet,
-                 mixins.ListModelMixin,
-                 mixins.CreateModelMixin):
-
-    """Manage tags in the database """
+class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
+                            mixins.ListModelMixin,
+                            mixins.CreateModelMixin):
+    """Base viewset for user owned attr."""
 
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        """Create a new object"""
+        serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user only"""
+
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class TagViewSet(BaseRecipeAttrViewSet):
+
+    """Manage tags in the database """
 
     queryset = Tag.objects.all()
     serializer_class = serializers.TagSerializers
 
-    def perform_create(self, serializer):
-        """Create a new tag"""
 
-        serializer.save(user=self.request.user)
-
-    def get_queryset(self):
-        """Return objects for the current authenticated user only"""
-
-        return self.queryset.filter(user=self.request.user).order_by('-name')
-
-
-class IngredientViewSet(viewsets.GenericViewSet,
-                        mixins.ListModelMixin,
-                        mixins.CreateModelMixin):
+class IngredientViewSet(BaseRecipeAttrViewSet):
 
     """Manage ingredients in the database"""
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+
     queryset = Ingredient.objects.all()
     serializer_class = serializers.IngredientSerializer
-
-    def perform_create(self, serializer):
-        """Create a new ingredient"""
-
-        serializer.save(user=self.request.user)
-
-    def get_queryset(self):
-        """Return objects for the current authenticated user only"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
