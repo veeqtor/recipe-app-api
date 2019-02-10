26609f8f1150ec6d16abc4cd9ae3949f08f62d7a
Veeqtor <nwokeochavictor@gmail.com>
2019-02-10 19:22:16 +0100

    ft(recipe): add filtering for recipe

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -49,6 +49,30 @@ class RecipeViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
+    def _params_to_ints(self, qs):
+        """"Convert a list of string IDs to a list of integers"""
+
+        return [int(str_id) for str_id in qs.split(',')]
+
+    def get_queryset(self):
+        """Retrieve the recipes for the authenticated user"""
+
+        tags = self.request.query_params.get('tags')
+        ingredients = self.request.query_params.get('ingredients')
+        queryset = self.queryset
+
+        if tags:
+            tag_ids = self._params_to_ints(tags)
+            # returns all whose ID is in the provide tag_ids
+            queryset = queryset.filter(tags__id__in=tag_ids)
+
+        if ingredients:
+            ingredient_ids = self._params_to_ints(ingredients)
+            # returns all whose ID is in the provide tag_ids
+            queryset = queryset.filter(ingredients__id__in=ingredient_ids)
+
+        return queryset.filter(user=self.request.user)
+
     def get_serializer_class(self):
         """Return appropriate serializer class"""
         if self.action == 'retrieve':
@@ -62,11 +86,6 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Create a new recipe"""
 
         serializer.save(user=self.request.user)
-
-    def get_queryset(self):
-        """Retrieve the recipes for the authenticated user"""
-
-        return self.queryset.filter(user=self.request.user)
 
     @action(methods=['POST'], detail=True, url_path='upload_image')
     def upload_image(self, request, pk=None):
