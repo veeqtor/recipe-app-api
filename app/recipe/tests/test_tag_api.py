ca2b6ec0a29ebedb9e648a9f549eafc1c5d3e238
Veeqtor <nwokeochavictor@gmail.com>
2019-02-11 10:52:41 +0100

    ft(filtering): Added filtering for ingredients and tags :fire:

--- app/recipe/tests/test_tag_api.py
+++ app/recipe/tests/test_tag_api.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 from rest_framework.test import APIClient
 from rest_framework import status
 
-from core.models import Tag
+from core.models import Tag, Recipe
 from recipe.serializers import TagSerializers
 
 TAGS_URL = reverse('recipe:tag-list')
@@ -86,3 +86,22 @@ class PrivateTagAPI(TestCase):
         res = self.client.post(TAGS_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_ingredients_assigned_to_recipes(self):
+        """Test filtering tags by those assigned to recipes"""
+
+        tag1 = Tag.objects.create(user=self.user, name='Breakfast')
+        tag2 = Tag.objects.create(user=self.user, name='Lunch')
+        recipe = Recipe.objects.create(
+            title='Coriander eggs on toast',
+            time_minutes=10,
+            price=5.00,
+            user=self.user,
+        )
+
+        recipe.tags.add(tag1)
+        res = self.client.get(TAGS_URL, {'assigned_only': 1})
+        serializer1 = TagSerializers(tag1)
+        serializer2 = TagSerializers(tag2)
+        self.assertIn(serializer1.data, res.data)
+        self.assertNotIn(serializer2.data, res.data)
